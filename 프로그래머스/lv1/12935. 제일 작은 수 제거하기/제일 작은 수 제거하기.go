926afeea87b2bfeabd1d6b53e3fb3545945b06a2
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-05-29 18:35:48 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.14 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.go
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.go
@@ -1,39 +1,26 @@
+
+import (
+	"sort"
+)
 func solution(arr []int) []int {
-    ret := []int{}
-    min := arr[0]
-    minIdx := 0
-    minIdxs := []int{}
-    if len(arr) <= 1 {
-        ret = append(ret, -1)
-        return ret
-    }
-    for i := 1; i < len(arr); i++ {
-        if arr[i] < min {
-            min = arr[i]
-            minIdx = i
-            minIdxs = []int{}
-            minIdxs = append(minIdxs, i)
-        } else if min == arr[i] {
-            minIdxs = append(minIdxs, i)
-        }
+	if len(arr) <= 1 {
+		return []int{-1}
+	} else {
+		var tmp = make([]int, len(arr))
+		copy(tmp, arr)
+		sort.Ints(tmp)
+		var min = tmp[0]
+		var appended = false
+		var returns = []int{}
+		for _, v := range arr {
+			if v == min && !appended {
+				appended = true
+				continue
+			} else {
+				returns = append(returns, v)
+			}
+		}
 
-    }
-
-    if len(minIdxs) > 1 {
-        for i := 0; i < len(minIdxs); i++ {
-            tmp := []int{}
-            if i > 0 {
-                tmp = arr[minIdxs[i-1]+1 : minIdxs[i]]
-
-            } else {
-                tmp = append(tmp, arr[:minIdxs[i]]...)
-
-            }
-            ret = append(ret, tmp...)
-        }
-        ret = append(ret, arr[minIdxs[len(minIdxs)-1]+1:]...)
-    } else {
-        ret = append(arr[:minIdx], arr[minIdx+1:]...)
-    }
-    return ret
+		return returns
+	}
 }
