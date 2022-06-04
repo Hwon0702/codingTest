d23f2a7e1b2ad53c6f7f810943b0baff525f0493
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-06-04 16:36:56 +0900

    [Silver III] Title: 2×n 타일링, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -3,11 +3,11 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
 N = int(input())
-result = [0]*(1001)
+result = [0]*(1001) #  1넣으면 에러나서 그냥 최대값 잡아버림
 result[0]=0
 result[1]=1
 result[2]=2
 if N>=3:
     for i in range(3,N+1):
-        result[i]=result[i-2]+result[i-1]
+        result[i]=result[i-2]%10007+result[i-1]%10007
 print(result[N]%10007)
