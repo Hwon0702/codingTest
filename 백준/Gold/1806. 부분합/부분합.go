0cc2f48e9682cab407342a368c794cf7a47f8aa6
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-07-01 23:26:22 +0900

    [Gold IV] Title: 부분합, Time: 1860 ms, Memory: 2132 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n, s, min, sum int
+	min = 100000002
+	fmt.Fscanf(reader, "%d %d\n", &n, &s)
+	var numbers = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscanf(reader, "%d ", &numbers[i])
+	}
+	for i := 0; i < n; i++ {
+		sum = numbers[i]
+		if sum >= s {
+			min = 1
+			break
+		}
+
+		for j := i + 1; j < n; j++ {
+			if sum >= s {
+				if j-i < min {
+					min = j - i
+				}
+				break
+			}
+			sum += numbers[j]
+			if sum >= s {
+				if j-i < min {
+					min = j - i + 1
+				}
+				break
+			}
+		}
+
+		if min == 1 {
+			break
+		}
+	}
+	if min >= 100000002 {
+		fmt.Fprintf(writer, "%d\n", 0)
+	} else {
+		fmt.Fprintf(writer, "%d\n", min)
+	}
+}
