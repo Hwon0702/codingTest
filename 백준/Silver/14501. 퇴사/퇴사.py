4fe2737f1f59f9e73c0f3cb5b0b65119d0037b80
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-07-07 00:49:54 +0900

    [Silver III] Title: 퇴사, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,18 @@
+import sys 
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+n = int(input())
+time, profit = [0 for _ in range(n+1)], [0 for _ in range(n+1)]
+for i in range(n):
+    t,p = map(int, input().split())
+    time[i] = t
+    profit[i] = p
+
+max_profit =[0 for _ in range(n+1)]
+
+for i in range(len(time)-2, -1, -1):  
+    if time[i]+i <= n:      
+        max_profit[i] = max(profit[i] + max_profit[i + time[i]], max_profit[i+1])   
+    else:               
+        max_profit[i] = max_profit[i+1]
+print(max_profit[0])
