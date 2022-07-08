a2bdcad70513d0eb6660a566020d2d2d48a1b8ab
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-07-08 22:03:43 +0900

    [Silver II] Title: A → B, Time: 4 ms, Memory: 900 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.go
+++ 백준/Silver/16953. A → B/A → B.go
@@ -4,32 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
 func mod(n int) int {
 	return n / 2
 }
 
-func minus(n int) (ret int) {
-	num := strconv.Itoa(n)
-	numArr := strings.Split(num, "")
-	if numArr[len(numArr)-1] == "1" {
-		numArr = numArr[:len(numArr)-1] //1을 더함
-
-		ret, _ = strconv.Atoi(strings.Join(numArr, ""))
-		return ret
+func minus(n int) int {
+	if n%10 == 1 {
+		return n / 10
 	} else {
 		return 0
 	}
-
 }
 
-/*
-끝이 2의 배수 -> 마지막 연산이 *2
-끝이 2의 배수가 아님 -> 마지막 연산은 +1
-*/
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
