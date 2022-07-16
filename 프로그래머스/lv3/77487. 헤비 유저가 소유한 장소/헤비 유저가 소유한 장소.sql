df21f1f6c3f85fb960c3a7f8b472155e7876e2f7
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-07-16 17:15:59 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,14 +1,15 @@
 -- 코드를 입력하세요
 SELECT 
-    PLACES.ID
+     PLACES.ID
     ,PLACES.NAME
     ,PLACES.HOST_ID
 FROM PLACES
 INNER JOIN (
-SELECT
-     COUNT(HOST_ID) AS count_host_id
-     ,HOST_ID
-FROM PLACES
-GROUP BY PLACES.HOST_ID
-)AS place ON place.HOST_ID=PLACES.HOST_ID AND place.count_host_id>=2
+    SELECT
+         COUNT(HOST_ID) AS count_host_id
+        ,HOST_ID
+    FROM PLACES
+    GROUP BY PLACES.HOST_ID
+    HAVING count_host_id>=2
+)AS place ON place.HOST_ID=PLACES.HOST_ID
 ORDER BY PLACES.ID ASC
