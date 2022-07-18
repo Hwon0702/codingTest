a2a9daa5c98a7b1a5d98034add3d544ec6be21b9
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-07-18 21:03:09 +0900

    [Silver II] Title: N과 M (11), Time: 896 ms, Memory: 66820 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,11 @@
+import sys 
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+from itertools import product
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+permutations_list = product(numbers, repeat=m)
+permutations_list = list(set(permutations_list))
+permutations_list.sort()
+for v in permutations_list:
+    print(' '.join(map(str, v)))
