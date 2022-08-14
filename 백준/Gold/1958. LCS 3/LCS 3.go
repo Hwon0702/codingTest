4c9859eb5c203ab9c067efb6e540a78100dc3469
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-08-14 20:50:49 +0900

    [Gold III] Title: LCS 3, Time: 40 ms, Memory: 15824 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.go
+++ 백준/Gold/1958. LCS 3/LCS 3.go
@@ -5,27 +5,23 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	var s1, s2, s3 string
 	fmt.Fscanf(reader, "%s\n%s\n%s", &s1, &s2, &s3)
-	s1Arr := strings.Split(s1, "")
-	s2Arr := strings.Split(s2, "")
-	s3Arr := strings.Split(s3, "")
-	var res = make([][][]float64, len(s1Arr)+1)
-	for i := 0; i <= len(s1Arr); i++ {
-		res[i] = make([][]float64, len(s2Arr)+1)
-		for j := 0; j <= len(s2Arr); j++ {
-			res[i][j] = make([]float64, len(s3Arr)+1)
+	var res = make([][][]float64, len(s1)+1)
+	for i := 0; i <= len(s1); i++ {
+		res[i] = make([][]float64, len(s2)+1)
+		for j := 0; j <= len(s2); j++ {
+			res[i][j] = make([]float64, len(s3)+1)
 		}
 	}
-	for i := 1; i <= len(s1Arr); i++ {
-		for j := 1; j <= len(s2Arr); j++ {
-			for k := 1; k <= len(s3Arr); k++ {
-				if s1Arr[i-1] == s2Arr[j-1] && s2Arr[j-1] == s3Arr[k-1] && s1Arr[i-1] == s3Arr[k-1] {
+	for i := 1; i <= len(s1); i++ {
+		for j := 1; j <= len(s2); j++ {
+			for k := 1; k <= len(s3); k++ {
+				if s1[i-1] == s2[j-1] && s2[j-1] == s3[k-1] && s1[i-1] == s3[k-1] {
 					res[i][j][k] = res[i-1][j-1][k-1] + 1
 				} else {
 					res[i][j][k] = math.Max(res[i-1][j][k], math.Max(res[i][j-1][k], res[i][j][k-1]))
@@ -33,5 +29,5 @@ func main() {
 			}
 		}
 	}
-	fmt.Printf("%0.f\n", res[len(s1Arr)][len(s2Arr)][len(s3Arr)])
+	fmt.Printf("%0.f\n", res[len(s1)][len(s2)][len(s3)])
 }
