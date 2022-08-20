4984f5732a823e6f32f9fecf3ba5593bb4a3d433
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-08-20 23:46:31 +0900

    [Gold IV] Title: 문자열 폭발, Time: 76 ms, Memory: 10564 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.go
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.go
@@ -7,31 +7,27 @@ import (
 	"strings"
 )
 
-type Stack struct {
-	Nodes []string
-}
-
-func (s *Stack) Push(c string) {
-	s.Nodes = append(s.Nodes, c)
-}
-
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	var s, rem string
 	fmt.Fscanf(reader, "%s\n%s", &s, &rem)
-	var stack = new(Stack)
-	sArr := strings.Split(s, "")
+	var stack = []byte{}
+	sArr := []byte(s)
 	remArr := strings.Split(rem, "")
 	var last = remArr[len(remArr)-1]
 	var length = len(remArr)
 	for _, s := range sArr {
-		stack.Push(s)
-		if s == last && len(stack.Nodes)-length >= 0 && strings.Join(stack.Nodes[len(stack.Nodes)-length:], "") == rem {
-			stack.Nodes = stack.Nodes[:len(stack.Nodes)-length]
+		stack = append(stack, s)
+		if len(stack) < length {
+			continue
+		} else if string(s) == last {
+			if string(stack[len(stack)-length:]) == rem {
+				stack = stack[:len(stack)-length]
+			}
 		}
 	}
-	if len(stack.Nodes) > 0 {
-		fmt.Println(strings.Join(stack.Nodes, ""))
+	if len(stack) > 0 {
+		fmt.Println(string(stack))
 	} else {
 		fmt.Println("FRULA")
 	}
