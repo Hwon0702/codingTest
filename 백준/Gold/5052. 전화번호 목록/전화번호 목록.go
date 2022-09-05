0ccc779a20b02a2d690fd60d3d0852f25eed7514
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-09-05 21:08:14 +0900

    [Gold IV] Title: 전화번호 목록, Time: 140 ms, Memory: 5352 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.go
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.go
@@ -13,21 +13,14 @@ var reader = bufio.NewReader(os.Stdin)
 func calculate(n int) {
 	var ret = "YES"
 	var strs = make([]string, n)
-	var breakFlag = false
 	for i := 0; i < n; i++ {
 		fmt.Fscanf(reader, "%s\n", &strs[i])
 	}
 	sort.Strings(strs)
 
-	for i := 0; i < n; i++ {
-		for j := i + 1; j < n; j++ {
-			if strings.HasPrefix(strs[j], strs[i]) {
-				ret = "NO"
-				breakFlag = true
-				break
-			}
-		}
-		if breakFlag {
+	for i := 0; i < n-1; i++ {
+		if strings.HasPrefix(strs[i+1], strs[i]) {
+			ret = "NO"
 			break
 		}
 	}
