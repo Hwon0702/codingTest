8db48d5eb1e9aed7ed9854f5cefd0b7f5717e824
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-09-11 21:39:05 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 632 ms, Memory: 36028 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -5,25 +5,25 @@ sys.setrecursionlimit(10**6)
 def bfs(s, e):
     visited = [-1]*100001
     q = deque()
-    q.append([s, 0])
+    q.append(s)
     visited[s]=0
     cnt=0
     while q:
-        now, time = q.popleft()
+        now = q.popleft()
         if now==e:
             cnt+=1
         if 0<=now*2<100001:
             if visited[now*2]==-1 or visited[now*2]==visited[now]+1:
                 visited[now*2]=visited[now]+1
-                q.append([now*2, time+1])
+                q.append(now*2)
         if 0<=now+1<100001:
             if visited[now+1]==-1 or visited[now+1]==visited[now]+1:
                 visited[now+1]=visited[now]+1
-                q.append([now+1, time+1])
+                q.append(now+1)
         if 0<=now-1<100001:
             if visited[now-1]==-1 or visited[now-1]==visited[now]+1:
                 visited[now-1]=visited[now]+1
-                q.append([now-1, time+1])
+                q.append(now-1)
     print(visited[e])
     print(cnt)      
 N, K = map(int, input().split())
