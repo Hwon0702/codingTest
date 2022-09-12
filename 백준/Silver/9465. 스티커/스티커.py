f814003a7e3953a76c6398785ecb0220ea2afa49
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-09-12 15:12:48 +0900

    [Silver I] Title: 스티커, Time: 1032 ms, Memory: 47040 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,20 @@
+import sys 
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+tc = int(input())
+for _ in range(tc):
+    width = int(input())
+    stickers = []
+    for _ in range(2):
+        stickers.append(list(map(int, input().split())))
+    if width<=1:
+        print(max(stickers[0][0], stickers[1][0]))
+    else:
+        res = stickers
+        res[0][1]+=res[1][0]
+        res[1][1]+=res[0][0]
+        for i in range(2, width):
+            res[0][i] = max(res[1][i-1] + stickers[0][i], res[1][i-2] + stickers[0][i])
+            res[1][i] = max(res[0][i-1] + stickers[1][i], res[0][i-2] + stickers[1][i])
+        print(max(res[0][width-1], res[1][width-1]))
