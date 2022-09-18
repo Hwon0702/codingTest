17505012e65be3de0077cf50a3697b8292714018
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-09-18 14:23:06 +0900

    [Bronze II] Title: 음계, Time: 68 ms, Memory: 32924 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,11 @@
+from audioop import reverse
+import sys 
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+a = list(map(int, input().split()))
+if a==sorted(a, reverse=False):
+    print("ascending")
+elif a==sorted(a, reverse=True):
+    print("descending")
+else:
+    print("mixed")
