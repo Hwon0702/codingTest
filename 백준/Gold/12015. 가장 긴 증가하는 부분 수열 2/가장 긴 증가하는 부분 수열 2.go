98462ebc785ce3aab3eaa4992adef1ed10a9247f
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-09-27 21:50:13 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 668 ms, Memory: 43752 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	var n, start, end, mid int
+	fmt.Fscanf(reader, "%d\n", &n)
+	var numbers = make([]int, n)
+	var nums = []int{0}
+	for i := 0; i < n; i++ {
+		fmt.Fscanf(reader, "%d ", &numbers[i])
+
+	}
+	for _, v := range numbers {
+		if nums[len(nums)-1] < v {
+			nums = append(nums, v)
+		} else {
+			start = 0
+			end = len(nums)
+			for start < end {
+				mid = int((start + end) / 2)
+				if nums[mid] < v {
+					start = mid + 1
+				} else {
+					end = mid
+				}
+			}
+			nums[end] = v
+		}
+	}
+	fmt.Println(len(nums) - 1)
+}
