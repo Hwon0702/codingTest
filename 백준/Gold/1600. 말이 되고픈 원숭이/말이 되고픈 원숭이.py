6ee0501ec77f1c2a8f6cc07a421a5a172f5fdd37
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-10-23 00:42:59 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 5324 ms, Memory: 49860 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -7,29 +7,29 @@ dy = [1, -1, 0, 0]
 hx = [-2, -2, -1, 1, 2, 2, -1, 1]
 hy = [-1, 1, -2, -2, -1, 1, 2, 2]
 k = int(input())
-m, n= map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+w, h= map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(h)]
 def bfs():
-    visited = [[[0]*(k+1) for _ in range(m)] for _ in range(n)]
+    visited = [[[0]*(k+1) for _ in range(w)] for _ in range(h)]
     queue = deque()
     queue.append((0,0,0))
     visited[0][0][0] = 1
 
     while queue:
         x, y, z = queue.popleft()
-        if x==n-1 and y==m-1:
+        if x==h-1 and y==w-1:
             return visited[x][y][z]-1
         for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
-            if 0<=nx<n and 0<=ny<m and not visited[nx][ny][z] and not graph[nx][ny]:
+            if 0<=nx<h and 0<=ny<w and not visited[nx][ny][z] and not graph[nx][ny]:
                 visited[nx][ny][z] = visited[x][y][z]+1
                 queue.append((nx,ny,z))
         if z<k:
             for i in range(8):
                 nx= x+hx[i]
                 ny = y+hy[i]
-                if 0<=nx<n and 0<=ny<m:
+                if 0<=nx<h and 0<=ny<w:
                     if not graph[nx][ny]:
                         if not visited[nx][ny][z+1]:
                             visited[nx][ny][z+1] = visited[x][y][z]+1
