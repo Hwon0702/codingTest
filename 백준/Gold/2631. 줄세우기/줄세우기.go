9be4591b216f50f05b18036b5645ee0715ddfa7f
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-11-05 23:52:21 +0900

    [Gold IV] Title: 줄세우기, Time: 8 ms, Memory: 912 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+var (
+	reader = bufio.NewReader(os.Stdin)
+	writer = bufio.NewWriter(os.Stdout)
+)
+
+func main() {
+	defer writer.Flush()
+	var n int
+	fmt.Fscanf(reader, "%d\n", &n)
+	var res = make([]float64, n)
+	var numbers = make([]float64, n)
+	var max float64
+	for i := 0; i < n; i++ {
+		res[i] = 1
+		fmt.Fscanf(reader, "%f\n", &numbers[i])
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			if numbers[j] < numbers[i] {
+				res[i] = math.Max(res[i], res[j]+1)
+			}
+		}
+	}
+	for _, v := range res {
+		if v > max {
+			max = math.Max(v, max)
+		}
+	}
+	fmt.Fprintf(writer, "%0.f", float64(n)-max)
+}
