edca99af2c9ec18c112b52dc70b09e949491d719
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2022-12-13 23:16:30 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 1100 ms, Memory: 40840 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 n, m  = map(int, input().split())
 
-frimes = [True for _ in range(1000001)]
+frimes = [True for _ in range(m-n+1)]
 for i in range(2, int(math.sqrt(m))+1):
     x = n//(i*i)
     if n%(i*i)!=0:
@@ -12,8 +12,4 @@ for i in range(2, int(math.sqrt(m))+1):
     while x*(i*i)<=m and 0<=x*(i*i)-n<m:
         frimes[x*(i*i)-n]=False
         x+=1
-res = 0
-for i in range(m-n+1):
-    if frimes[i]:
-        res+=1
-print(res)
+print(frimes.count(True))
