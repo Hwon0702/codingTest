6ce9a3160216fcc29995f13343d1f81808d64673
initi commit <>
2022-12-23 00:13:45 +0900

    no message

--- 백준/Gold/5557. 1학년/1학년.py
+++ 백준/Gold/5557. 1학년/1학년.py
@@ -7,9 +7,8 @@ dp = [[0 for _ in range(21)] for _ in range(n)]
 dp[0][nums[0]] = 1
 for i in range(1, n - 1):
     for j in range(21):
-        if dp[i - 1][j]:
-            if 0 <= j + nums[i] <= 20: 
-                dp[i][j + nums[i]] += dp[i - 1][j]
-            if 0 <= j - nums[i] <= 20: 
-                dp[i][j - nums[i]] += dp[i - 1][j]
+        if 0 <= j + nums[i] < 21: 
+            dp[i][j + nums[i]] += dp[i - 1][j]
+        if 0 <= j - nums[i] < 21: 
+            dp[i][j - nums[i]] += dp[i - 1][j]
 print(dp[n - 2][nums[-1]])
