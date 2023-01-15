2fc21d03a2fc6883450f081d8ba80e312b882d29
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2023-01-15 21:50:15 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 60 ms, Memory: 34152 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,31 @@
+import sys 
+from collections import deque
+input = sys.stdin.readline 
+sys.setrecursionlimit(10**6)
+n, m = map(int, input().split())
+graph = [set()for _ in range(n+1)]
+res = [999999 for _ in range(n+1)]
+def find(s):
+    q = deque()
+    cost = [999 for _ in range(n+1)]
+    visited = [False for _ in range(n+1)]
+    visited[0]=False
+    visited[s]=False
+    q.append(s)
+    cost[s]=0
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            if not visited[i]:
+                q.append(i)
+                visited[i]=True
+                cost[i]=min(cost[i], cost[now]+1)
+    return sum(cost[1:])
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    graph[s].add(e)
+    graph[e].add(s)
+for i in range(1, n+1):
+    res[i] =find(i)
+print(res.index(min(res)))
