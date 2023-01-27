a07026bcc8accf1622c1b1c8bfaa18482d524c71
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2023-01-27 20:11:38 +0900

    [Gold II] Title: 짝수번째 피보나치 수의 합, Time: 4 ms, Memory: 964 KB -BaekjoonHub

---
+++ 백준/Gold/11443. 짝수번째 피보나치 수의 합/짝수번째 피보나치 수의 합.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	matrix = [][]int{}
+)
+
+func mulMatrix(matrix1, matrix2 [][]int) [][]int {
+	res := [][]int{}
+	res = append(res, []int{0, 0})
+	res = append(res, []int{0, 0})
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			for k := 0; k < 2; k++ {
+				res[i][j] += matrix1[i][k] * matrix2[k][j] % 1000000007
+			}
+		}
+	}
+	return res
+}
+
+func power(a [][]int, b uint64) [][]int {
+	if b == 1 {
+		return a
+	}
+	tmp := power(a, b/2)
+	if b%2 == 0 {
+		return mulMatrix(tmp, tmp)
+	} else {
+		return mulMatrix(mulMatrix(tmp, tmp), a)
+	}
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	var n uint64
+	matrix = append(matrix, []int{1, 1})
+	matrix = append(matrix, []int{1, 0})
+	fmt.Fscanf(reader, "%d\n", &n)
+	if n%2 == 0 {
+		n = n + 1
+	}
+	result := power(matrix, n)
+	fmt.Fprintf(writer, "%d", (result[0][1]%1000000007)-1)
+}
