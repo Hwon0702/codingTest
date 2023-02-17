54d1cdb5524e124919a1b8fe765b722354263a85
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2023-02-17 22:42:42 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1524 ms, Memory: 77076 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -1,10 +1,10 @@
 import sys 
-import heapq
 input = sys.stdin.readline 
 sys.setrecursionlimit(10**6)
 n = int(input())
 num = []
 for _ in range(n):
-    heapq.heappush(num, -int(input()))
-while num:
-    print(-heapq.heappop(num))
+    num.append(int(input().strip('\n')))
+num.sort(reverse=True)
+for v in num:
+    print(v)
