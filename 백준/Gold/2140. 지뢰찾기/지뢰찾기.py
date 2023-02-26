0f66665307d4ef225f912b8ad6bb07a1e1d1b0b6
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2023-02-26 19:12:33 +0900

    [Gold IV] Title: 지뢰찾기, Time: 132 ms, Memory: 34232 KB -BaekjoonHub

---
+++ 백준/Gold/2140. 지뢰찾기/지뢰찾기.py
@@ -0,0 +1,45 @@
+import sys 
+from collections import deque
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+dx = [-1, -1, -1, 0, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
+
+n = int(input())
+q = deque()
+boards = []
+bomb = [[0 for _ in range(n)]for _ in range(n)]
+visited = [[False for _ in range(n)] for _ in range(n)]
+for i in range(n):
+    tmp = list(map(str, input().rstrip('\n')))
+    for j in range(len(tmp)):
+        if tmp[j]=='#':
+            q.append([i, j])
+            bomb[i][j]=float('inf') #일단 가려진 부분은 폭탄이 있다고 가정하기
+        else:
+            tmp[j]=int(tmp[j])
+    boards.append(tmp)
+while q:
+    cx, cy = q.popleft()
+    cnt = 0
+    for i in range(9):
+        nx = cx+dx[i]
+        ny = cy+dy[i]
+        cnt+=1
+        if 0<=nx<n and 0<=ny<n:
+            if boards[nx][ny]!='#':
+                bomb[cx][cy] = min(bomb[cx][cy], boards[nx][ny])
+    if bomb[cx][cy]>0:
+        for i in range(9):
+            nx = cx+dx[i]
+            ny = cy+dy[i]
+            if 0<=nx<n and 0<=ny<n :
+                if boards[nx][ny]!='#' and boards[nx][ny]>0:
+                    boards[nx][ny] -=1
+ 
+res = 0
+for v in bomb:
+    for k in v:
+        if k>0:
+            res+=1
+print(res)
