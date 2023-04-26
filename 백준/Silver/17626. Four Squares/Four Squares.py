1562480a0511e2d3e5d61eab81b1b8d151d410a7
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2023-04-26 23:07:52 +0900

    [Silver III] Title: Four Squares, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,32 @@
+import sys 
+input = sys.stdin.readline 
+sys.setrecursionlimit(10**6)
+squres = []
+
+for i in range(1, int(50001**0.5)+1):
+    squres.append(i**2)
+def find(n):
+    l, r = 0, len(squres)-1
+    res = False
+    while l<=r:
+        if squres[l]+squres[r]==n:
+            res= True
+            break
+        elif squres[l]+squres[r]>n:
+            r-=1
+        else:
+            l+=1
+    return res
+def squre(a):
+    if int(a**0.5)==a**0.5:
+        return 1
+    if find(a):
+        return 2
+    for v in squres: 
+        if find(n-v):
+            return 3
+    else:
+        return 4
+    
+n = int(input())
+print(squre(n))
