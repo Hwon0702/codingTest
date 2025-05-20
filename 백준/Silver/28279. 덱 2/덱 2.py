590cbe30dbb44e965aa89cda5e43dae3fd4ee07d
Hwon0702 <86557181+Hwon0702@users.noreply.github.com>
2025-05-21 01:12:38 +0900

    [Silver IV] Title: 덱 2, Time: 1568 ms, Memory: 72568 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -0,0 +1,26 @@
+import sys 
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+from collections import deque
+
+n = int(input())
+dequeue = deque()
+for i in range(n):
+    num = list(map(int, input().strip().split()))
+    l = len(dequeue)
+    if num[0] == 1:
+        dequeue.appendleft(num[1])
+    elif num[0] == 2:
+        dequeue.append(num[1])
+    elif num[0] == 3:
+        print(dequeue.popleft() if l else -1)
+    elif num[0] == 4:
+        print(dequeue.pop() if l else -1)
+    elif num[0] == 5:
+        print(len(dequeue))
+    elif num[0] == 6:
+        print(0 if l else 1)
+    elif num[0] == 7:
+        print(dequeue[0] if l else -1)
+    elif num[0] == 8:
+        print(dequeue[-1] if l else -1)
